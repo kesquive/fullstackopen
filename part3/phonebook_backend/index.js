b55c49fdc399a301140c2372e2314264ca66cfff
kesquive <karimejocelyn@gmail.com>
2023-07-14 15:35:16 -0600

    part3 - 3.6 phonebock_backend

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -69,6 +69,19 @@ app.delete("/api/persons/:id", (request, response) => {
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const existPerson = persons.find((person) => person.name === body.name);
+
+  if (existPerson) {
+    return response.status(400).json({
+      error: "Name must be unique",
+    });
+  }
   const person = {
     id: generateId(),
     name: body.name,
