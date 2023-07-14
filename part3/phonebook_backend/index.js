577117dc879a0ab27986a6b12d3dd1bd5cb30036
kesquive <karimejocelyn@gmail.com>
2023-07-14 13:24:37 -0600

    part3 - 3.3 phonebock_backend

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -42,5 +42,17 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.statusMessage = "Person dont found";
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT);
