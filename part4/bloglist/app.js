574bb5e8a23519f05e3d0f59152a11b0b7c4c4dc
kesquive <karimejocelyn@gmail.com>
2023-09-28 16:25:35 -0600

    4.15: bloglist expansion, step3

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 const logger = require("./utils/logger");
 const middleware = require("./utils/middleware");
 const mongoose = require("mongoose");
@@ -21,6 +22,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 app.use(middleware.errorHandler);
 
 module.exports = app;
