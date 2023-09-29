3f8a404124ac15c00401ce9a89bf3c6a76289733
kesquive <karimejocelyn@gmail.com>
2023-09-29 15:54:55 -0600

    4.21 bloglist expansion step9

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -40,8 +40,20 @@ blogsRouter.post("/", async (request, response, next) => {
 
 blogsRouter.delete("/:id", async (request, response, next) => {
   try {
-    await Blog.findByIdAndRemove(request.params.id);
-    response.status(204).end();
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
+
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: "invalid token" });
+    }
+
+    const blog = await Blog.findById(request.params.id);
+
+    if (blog.user.toString() === decodedToken.id.toString()) {
+      await Blog.findByIdAndRemove(request.params.id);
+      response.status(204).end();
+    } else {
+      response.status(401).end();
+    }
   } catch (exception) {
     next(exception);
   }
