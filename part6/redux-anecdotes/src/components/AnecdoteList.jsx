17cee095b5d09b2e1596cfea8f3e1bf1c217f681
kesquive <karimejocelyn@gmail.com>
2023-12-01 17:03:02 -0600

    6.10 better anecdotes step8

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -15,16 +15,12 @@ const Anecdote = ({ anecdote, handleClick }) => {
 
 const AnecdoteList = (props) => {
   const anecdotes = useSelector(({ filter, anecdotes }) => {
-    if (filter === "ALL") {
-      return anecdotes.sort((a, b) => {
-        return b.votes - a.votes;
-      });
+    if (filter === null) {
+      return anecdotes.slice().sort((a, b) => b.votes - a.votes);
     } else {
       return anecdotes
         .filter((anecdote) => anecdote.content.includes(filter))
-        .sort((a, b) => {
-          return b.votes - a.votes;
-        });
+        .sort((a, b) => b.votes - a.votes);
     }
   });
   const dispatch = useDispatch();
